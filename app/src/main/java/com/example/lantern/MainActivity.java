4ec5b2ec9af2a06715d39b86c2880e0b7c40546e
MadMakZa <immakza@gmail.com>
2021-01-01 19:09:11 +0300

    main activity

--- app/src/main/java/com/example/lantern/MainActivity.java
+++ app/src/main/java/com/example/lantern/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.lantern;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button bFlash;
+    private FlashClass mFlashClass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private void init(){
+        bFlash = findViewById(R.id.b1);
+        mFlashClass = new FlashClass(this);
+    }
+    public void onClickFlash(View view){
+        if(mFlashClass.isFlash_status()){
+            mFlashClass.flashOff();
+        }else{
+            mFlashClass.flashOn();
+        }
     }
 }
