1c049b0a8f6da436b70fed83a7b4e0c0ba5a1709
MadMakZa <immakza@gmail.com>
2021-01-16 22:56:34 +0300

    add method blink, work version

--- app/src/main/java/com/example/lantern/MainActivity.java
+++ app/src/main/java/com/example/lantern/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     }
     //вспышка
     public void onClickBlink(View view){
-        mFlashClass.blink();
+            mFlashClass.blink();
+
     }
 }
